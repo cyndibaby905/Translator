fffaf58b4dfbfe2033cccbfb342ec86a891dd899
Hang Chen <cyndibaby905@yahoo.com.cn>
2012-11-28 18:48:43 +0800

    Support command parameters

--- translator.py
+++ translator.py
@@ -2,6 +2,7 @@ import urllib2
 import json
 import codecs
 import re
+import sys
 
 def getPage(words,src,dest):
 	url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20google.translate%20where%20q%3D%22" + words +"%22%20and%20target%3D%22" + dest + "%22%20and%20source%3D%22" + src +"%22%3B&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback="
@@ -23,22 +24,32 @@ def parseJsonResult(resultStr):
 	
  
 if __name__ == "__main__":
+	fileName = "Localizable.strings"
+	destLanguage = "zh"
+	srcLanguage = "en"
+	if len(sys.argv) >= 4:
+		fileName = sys.argv[1]
+		srcLanguage = sys.argv[2]
+		destLanguage = sys.argv[3]
 	
-	f = codecs.open("Localizable.strings", "r", "utf-16")
+	
+	
+	
+	f = codecs.open(fileName, "r", "utf-16")
 	for line in f:
 		#The translation key
-		pattern = re.compile(r'[\"\'].*[\"\'] = [\"\'].*[\"\']')
+		pattern = re.compile(r'[\"\'].*[\"\']\s*=\s*[\"\'].*[\"\']')
 		match = pattern.match(line)
 		if match:
 			src = line.split("=")[0].strip()
-			namesPage = getPage(urllib2.quote(src[1:-1].encode('utf8')),'en','zh')
+			namesPage = getPage(urllib2.quote(src[1:-1].encode('utf8')),srcLanguage,destLanguage)
 			result = json.loads(namesPage)
 			print src[1:-1] + ":" + parseJsonResult(namesPage)
 			
 	
 
 	srcStr = """There are several ways to present the output of a program; data can be printed in a human-readable form, or written to a file for future use. This chapter will discuss some of the possibilities."""
-	namesPage = getPage(urllib2.quote(srcStr.encode('utf8')),'en','zh')
+	namesPage = getPage(urllib2.quote(srcStr.encode('utf8')),srcLanguage,destLanguage)
 	result = json.loads(namesPage)
 	
 	print parseJsonResult(namesPage)
